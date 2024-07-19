82353f1b53a6ba7f38c14ad6e497ccf84d56d242
anyel-ec <paulangel826@gmail.com>
2024-07-18 22:45:46 -0500

    add crud of user, connection to MongoDB

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-const http = require('node:http');
+const express = require('express');
+const EnvLoader = require('./src/config/environment');
+const { connectDB } = require('./src/config/db');
+const userRoutes = require('./src/routes/userRoutes');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const envLoader = new EnvLoader(); // Cargar variables de entorno desde el archivo .env
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+const app = express();
+app.use(express.json());
+
+const PORT = envLoader.get('PORT') || 3000;
+
+// Conectar a la base de datos
+connectDB().then(() => {
+  // Definir las rutas
+  app.use('/api/users', userRoutes);
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}).catch(err => {
+  console.error('Failed to connect to MongoDB', err);
 });
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-}); 
