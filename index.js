f15eed6994454a91224edda578e8f4d0b30fc270
anyel-ec <paulangel826@gmail.com>
2024-07-27 16:52:44 -0500

    enable cors

--- index.js
+++ index.js
@@ -12,15 +12,20 @@ const app = express();
 app.use(express.json());
 
 const PORT = envLoader.get('PORT') || 3000;
+//  allow access on origins
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
+  next();
+});
 // Conectar a la base de datos
 connectDB().then(() => {
   // Definir las rutas
   app.use('/api/registers', registerRoutes);
   app.use('/api/verifyData', verifyDataRoutes); 
-  app.use('/api/load', loadDataRoutes);
-
-  
+  app.use('/api/load', loadDataRoutes); 
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
